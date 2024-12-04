785541a16b37008ec2d79b63f906df34a565b2d8
Ishaan400 <wiseman20044@gmail.com>
2024-12-04 11:30:41 +0530

    Add files via upload
    first

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose");
+
+const userSchema=mongoose.Schema({
+    user:{ type: mongoose.Schema.Types.ObjectId,
+           ref: "user"
+         },
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        content: String,
+        likes: [
+            {type: mongoose.Schema.Types.ObjectId,
+                ref:"user"
+             }
+        ]
+   } );
